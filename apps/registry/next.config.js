fea24b272914a52eec1c03954b7fb2a4af299fad
Hoang Truong <tvhoang91@gmail.com>
2025-08-05 14:06:39 +0700

    add register

---
+++ apps/registry/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig;
